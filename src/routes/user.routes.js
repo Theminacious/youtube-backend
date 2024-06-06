b6122c962360f9893500109f76138799dd799bb7
theminacious <zacksunrise32@gmail.com>
2024-06-06 19:59:46 +0530

    "Added new routes to user.routes.js for changing password, getting current user, updating account details, updating user avatar and cover image, getting user channel profile, and getting watch history."

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,7 +1,19 @@
 import {Router} from 'express'
-import {loginUser, logoutUser, refreshAccessToken, registerUser} from '../controllers/user.controller.js'
+import {changeCurrentPassword,
+     getCurrentUser, 
+     getUserChannelProfile,
+      getWatchHistory, 
+      loginUser,
+       logoutUser,
+        refreshAccessToken, 
+       registerUser,
+        updateAccountDetails, 
+        updateUserAvatar,
+         updateUserCoverImage
+        } from '../controllers/user.controller.js'
 import {upload} from '../middlewares/multer.middleware.js'
 import { verifyJwt } from '../middlewares/auth.middleware.js'
+
 
 
 const router = Router()
@@ -26,6 +38,21 @@ router.route('/login').post(loginUser)
 router.route('/logout').post(verifyJwt,logoutUser)
 
 router.route('/refresh-token').post(refreshAccessToken)
+
+router.route('/change-password').post(verifyJwt,changeCurrentPassword)
+
+router.route('/current-password').get(verifyJwt,getCurrentUser)
+
+router.route('/update-account').patch(verifyJwt,updateAccountDetails)
+
+router.route('/avatar').patch(verifyJwt,upload.single("avatar"),updateUserAvatar)
+
+router.route('/cover-image').patch(verifyJwt,upload.single('coverImage'),updateUserCoverImage)
+
+router.route('/c/:username').get(verifyJwt,getUserChannelProfile)
+
+router.route('/history').get(verifyJwt,getWatchHistory)
+
 
 
 export default router
