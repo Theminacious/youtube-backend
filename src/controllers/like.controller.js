9efed620d5db0c41fa34a19b6a9c3c12f23ae0a7
theminacious <zacksunrise32@gmail.com>
2024-06-09 01:34:50 +0530

    "Implemented like functionality for comments and tweets, and added getLikedVideos endpoint"

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -4,6 +4,7 @@ import {ApiError} from "../utils/ApiError.js"
 import {ApiResponse} from "../utils/ApiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
 import { Video } from "../models/video.models.js"
+import { Tweet } from "../models/tweets.models.js"
 
 const toggleVideoLike = asyncHandler(async (req, res) => {
     const { videoId } = req.params;
@@ -42,20 +43,96 @@ const toggleVideoLike = asyncHandler(async (req, res) => {
 });
 
 const toggleCommentLike = asyncHandler(async (req, res) => {
-    const {commentId} = req.params
-    //TODO: toggle like on comment
+    const { commentId } = req.params;
 
-})
+    // Validate commentId
+    if (!isValidObjectId(commentId)) {
+        throw new ApiError(400, "Invalid comment ID");
+    }
 
+    // Find the comment by its ID
+    const comment = await Comment.findById(commentId);
+
+    // Check if the comment exists
+    if (!comment) {
+        throw new ApiError(404, "Comment not found");
+    }
+
+    // Check if the user has already liked the comment
+    const userId = req.user._id.toString();
+    const likeIndex = comment.likes.indexOf(userId);
+
+    if (likeIndex === -1) {
+        // User has not liked the comment, add like
+        comment.likes.push(userId);
+    } else {
+        // User has liked the comment, remove like
+        comment.likes.splice(likeIndex, 1);
+    }
+
+    // Save the updated comment
+    await comment.save();
+
+    // Return the appropriate response
+    const action = likeIndex === -1 ? "liked" : "unliked";
+    return res.status(200).json(new ApiResponse(200, comment.likes, `Comment ${action} successfully`));
+});
 const toggleTweetLike = asyncHandler(async (req, res) => {
     const {tweetId} = req.params
     //TODO: toggle like on tweet
+
+if(!isValidObjectId(tweetId)){
+    throw new ApiError(400, "Invalid tweet ID")
 }
-)
+
+
+const tweet = await Tweet.findById(tweetId)
+
+if(!tweet){
+    throw new ApiError(404, "Tweet not found")
+
+}
+
+const userId = req.user._id.toString();
+const likeIndex = tweet.likes.indexOf(userId);
+
+if (likeIndex === -1) {
+    // User has not liked the comment, add like
+    tweet.likes.push(userId);
+} else {
+    // User has liked the comment, remove like
+    tweet.likes.splice(likeIndex, 1);
+}
+
+// Save the updated comment
+await tweet.save();
+
+// Return the appropriate response
+const action = likeIndex === -1 ? "liked" : "unliked";
+return res.status(200).json(new ApiResponse(200, tweet.likes, `Tweet ${action} successfully`));
+
+})
 
 const getLikedVideos = asyncHandler(async (req, res) => {
-    //TODO: get all liked videos
-})
+    const userId = req.user._id;
+
+    try {
+        const videos = await Video.find({ likes: { $in: [userId] } })
+            .populate({
+                path: 'owner',
+                select: 'username avatar'
+            });
+
+        if (!videos.length) {
+            throw new ApiError(404, "No liked videos found");
+        }
+
+        return res.status(200).json(new ApiResponse(200, videos, "Liked videos fetched successfully"));
+    } catch (error) {
+        console.error("Error fetching liked videos:", error);
+        throw new ApiError(500, "Internal server error");
+    }
+});
 
 export {
     toggleCommentLike,
